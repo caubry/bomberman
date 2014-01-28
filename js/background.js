9be60e430ebb405c3050be9ad7b8c94d88566dfc
Caroline Aubry <caroline@carolineaubry.com>
2014-01-28 23:16:45 +0000

    Control the placement of tiles

--- js/background.js
+++ js/background.js
@@ -6,10 +6,9 @@ Background = Class.extend({
   layers: [],
 
   /**
-  * From the JSON file data, set the properties of each layer
-  * and draw to the canvas.
+  * Draw to the canvas.
   */
-  renderLayer: function(layer) {
+  drawTiles: function(layer) {
     if (layer.type !== "tilelayer" || !layer.opacity) { return; }
     size = game.background.data.tilewidth;
 
@@ -25,12 +24,53 @@ Background = Class.extend({
 
         s_x = (i % layer.width) * size;
         s_y = Math.floor(i / layer.width) * size;
-       
         game.ctx.drawImage(game.background.tileset, img_x, img_y, size, size, s_x, s_y, size, size);
       });
-      
-      game.background.layers.push(game.ctx.canvas.toDataURL());
-      game.ctx.drawImage(game.ctx.canvas, 0, 0);
+    }
+  },
+
+  /**
+  * From the JSON file data, set the properties of each layer
+  */
+  renderLayer: function(layer) {
+    for (var key in layer)
+    {
+      switch(layer[key]) {
+        case "static-blocks": {
+          game.background.drawTiles(layer);
+        }
+        break;
+        case "despicable-block": {
+          console.log('despicable-block: ')
+          layer.data[1] = 1;
+          layer.data[2] = 1;
+          layer.data[3] = 1;
+          console.log(layer.data[1])
+          if (layer.type !== "tilelayer" || !layer.opacity) { return; }
+          size = game.background.data.tilewidth;
+
+          if (game.background.layers.length < game.background.data.layers.length) {
+            layer.data.forEach(function(tile_idx, i) {
+              if (!tile_idx) { return; }
+              var img_x, img_y, s_x, s_y,
+              tile = game.background.data.tilesets[0];
+              tile_idx--;        
+
+              img_x = (tile_idx % (tile.imagewidth / size)) * size;
+              img_y = Math.floor(tile_idx / (tile.imagewidth / size)) * size;
+
+              s_x = (i * 1 % layer.width) * size;
+              s_y = (i * 1 % layer.width) * size;
+              game.ctx.drawImage(game.background.tileset, img_x, img_y, size, size, s_x, s_y, size, size);
+            });
+          }
+        }
+        break;
+        case "despicable-collision": {
+          console.log('despicable-collision')
+        }
+        break;
+        }
     }
   },
 
